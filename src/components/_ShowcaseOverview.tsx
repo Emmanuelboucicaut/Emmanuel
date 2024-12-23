f94b1f45a97cffc7bc1e41109e3334cc8a8f88a9
erejitservices <erejitservices@gmail.com>
2024-12-22 21:33:53 -0500

    modify showcase file

--- src/components/_ShowcaseOverview.tsx
+++ src/components/_ShowcaseOverview.tsx
@@ -1,10 +1,13 @@
 const ShowcaseOverview = () => {
   return (
-    <p className="lead my-4">
-      I am a .Net Developer with 12 years of experience in software development.
-      I have a strong background in C#/.Net and have worked on a variety of
-      projects, from web applications to desktop applications. I am passionate
-      about creating high-quality software that meets the needs of my clients.
+    <p className="my-4 w-60 text-lg font-light leading-relaxed text-gray-700 md:w-80">
+      12 years of prior experience as a data analyst, SQL developer, and .NET
+      developer,Deep understanding of ETL Tools, Best Practice Processes,Data
+      Movement Tools (SSIS, Talend),Proficient in SQL (SQL, Stored Procedures,
+      Dynamic SQL, Joins, Aggregation), Experience with several Database Systems
+      (Oracle, SQL Server, MySQL, Postgres, MongoDB),Design, Develop, and
+      Deployment Best Practices, Understanding of Business Intelligence Best
+      Practices, Architectures, Tools, Methodologies, and Software.
     </p>
   );
 };
